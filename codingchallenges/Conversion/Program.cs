7d225adfd417032abedf7b565c7652314bc8e673
Akil Tomlinson <akil.tomlinson@revature.net>
2021-09-14 01:19:48 -0400

    Adding conversion challenge

---
+++ codingchallenges/Conversion/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace Conversion
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      //Casting
+      //Does not round up or recognize anu numbers after the 
+      int cast = (int)99.99f;
+      Console.WriteLine(cast);
+
+      //Implicit Conversion
+      int convertInt = 26;
+      float newFloat = convertInt;
+      Console.WriteLine(newFloat);
+
+      //Convert 
+      //Converts one data type to another
+      string date = "2021";
+      int convertDate = Convert.ToInt32(date);
+      Console.WriteLine(convertDate);
+
+      //Parse
+      //Takes strings
+      //Throws exception if null
+      //No support for customer types 
+      int parseDate = int.Parse(date);
+      Console.WriteLine(parseDate);
+
+
+    }
+  }
+}
